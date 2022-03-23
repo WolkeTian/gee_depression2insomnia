8085e175870a441e5799a3a9c1bf0f0fb56e7158
Yun Tian <sherfly.t@gmail.com>
2022-03-23 15:57:23 +0800

    Create gee_isi.R

---
+++ gee_isi.R
@@ -0,0 +1,13 @@
+modeldata = read.csv("insomnia_fnc_gee.csv", header = TRUE)
+head(modeldata)
+
+library(geepack)
+gee_isi = geeglm(formula = isi ~ bmi + gender + sds +
+                   anxiety + meq + first +isi_base,
+                  family = gaussian, data = modeldata, 
+                 id = subid, corstr = "ar1")
+
+summary(gee_isi)
+
+# compare qic
+QIC(gee_isi)
